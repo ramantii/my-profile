c9710514a56e140de784b8fd3845b4d152ed0ada
ramantii <ramanti.prajna@gmail.com>
2024-04-04 22:23:53 +0700

    fix bug

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend('re_YdswEad3_NFHCxMsuHtxDKc5cyxEmw76S');
-const fromEmail = env('ramanti.prajna@gmail.com');
+const fromEmail = 'ramanti.prajna@gmail.com';
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
